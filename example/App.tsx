ea27a3d2a0eca710e454591173640b4468844ba5
uit2712 <phamquangvi456@gmail.com>
2021-05-31 16:41:16 +0700

    init typescript

---
+++ example/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+    View,
+} from 'react-native';
+
+function App() {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default App;
