031ebcf684e0ee60dfc791a5438175e0340854dc
uit2712 <phamquangvi456@gmail.com>
2021-05-31 20:30:31 +0700

    add component NavigatorExample

---
+++ template/NavigatorExample.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Drawer1Navigator from './navigators';
+
+function App() {
+    return (
+        <Drawer1Navigator />
+    );
+};
+
+export default App;
